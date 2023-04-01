ea85a0e09907f1422ce69632d3b2f2c5ae567abb
amelchenko_n <amelchenko_n@stilsoft.ru>
2023-04-01 21:02:44 +0300

    add styles, fonts, img and componens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import Categories from './components/Categories'
+import Header from './components/Header'
+import PizzaBlock from './components/PizzaBlock'
+import Sort from './components/Sort'
+import './styles/app.scss'
+
 function App() {
-    return <div className="App" />
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <Header />
+                <div className="content">
+                    <div className="container">
+                        <div className="content__top">
+                            <Categories />
+                            <Sort />
+                        </div>
+                        <h2 className="content__title">Все пиццы</h2>
+                        <div className="content__items">
+                            <PizzaBlock />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
