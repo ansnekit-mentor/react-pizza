3cf9d91c141443a68177098b928459a289aca859
amelchenko_n <amelchenko_n@stilsoft.ru>
2023-05-07 23:54:59 +0300

    add layout and page pizza detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
 import './styles/app.scss'
 import PageCart from './pages/PageCart'
 import Page404 from './pages/Page404'
